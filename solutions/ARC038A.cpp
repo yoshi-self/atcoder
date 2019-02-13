a9a4b349c8a7a24e6cbcccd926f97d68db54bb39
Yoshi <crispy245@gmail.com>
2019-02-13 22:21:38 +0900

    Add ARC038A.cpp

---
+++ solutions/ARC038A.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+//const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  int n;
+  scanf(" %d", &n);
+  vector<int> v(n);
+  int t;
+  REP(i, n) {
+    scanf(" %d", &t);
+    v.push_back(t);
+  }
+
+  int ans = 0;
+  sort(v.begin(), v.end(), greater<int>());
+  for(int i = 0; i < v.size(); i += 2) {
+    ans += v[i];
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
