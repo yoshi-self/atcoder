e11918a376da88e901618e0c438c405c4f2d05e4
Yoshi <crispy245@gmail.com>
2019-02-18 23:01:56 +0900

    Add ARC041A.cpp

---
+++ solutions/ARC041A.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+int main() {
+  LL x, y, k;
+  cin >> x >> y >> k;
+  LL ans = 0;
+  if(y >= k) {
+    ans = x + k;
+  }
+  else {
+    ans = y + x - (k - y);
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
