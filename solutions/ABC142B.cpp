a9341986824205eb8958a5721533b05284709c4d
Yoshi <crispy245@gmail.com>
2019-09-29 14:23:14 +0900

    Add ABC142B.cpp

---
+++ solutions/ABC142B.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+int main() {
+  LL n;
+  int k;
+  cin >> n >> k;
+  LL ans = 0;
+
+  REP(i, n) {
+    int tmp;
+    cin >> tmp;
+    if(tmp >= k) {
+      ans += 1;
+    }
+  }
+
+  cout << ans << '\n';
+  return 0;
+}
