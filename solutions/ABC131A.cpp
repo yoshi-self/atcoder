7c4abe7defffc5402a301accedeb6d912a9a8773
Yoshi <crispy245@gmail.com>
2019-09-30 11:24:50 +0900

    Add ABC131A.cpp

---
+++ solutions/ABC131A.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  char prev = s[0];
+  string ans = "Good";
+  FOR(i, 1, 4) {
+    char cur = s[i];
+    if(prev == cur) {
+      ans = "Bad";
+    }
+    prev = cur;
+  }
+  cout << ans << endl;
+  return 0;
+}
