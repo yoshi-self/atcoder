389ebe468f269f2bb720229c1cd59944ad88c3bf
Yoshi <crispy245@gmail.com>
2019-10-05 20:24:50 +0900

    Add ABC126A.cpp

---
+++ solutions/ABC126A.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  string s;
+  cin >> s;
+  s[k - 1] = tolower(s[k - 1]);
+  cout << s << '\n';
+  return 0;
+}
