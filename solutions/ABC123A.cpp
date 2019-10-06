ec1ddeab06e67d37067a5db4b0a9653954517571
Yoshi <crispy245@gmail.com>
2019-10-06 14:54:56 +0900

    Add ABC123A.cpp

---
+++ solutions/ABC123A.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+int main() {
+  int a, b, c, d, e, k;
+  cin >> a >> b >> c >> d >> e >> k;
+
+  if(e - a <= k) {
+    cout << "Yay!" << '\n';
+  }
+  else {
+    cout << ":(" << '\n';
+  }
+
+  return 0;
+}
