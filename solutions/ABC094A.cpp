08de03e9bc581031c27e833f0f838cabb5d99eb5
Yoshi <crispy245@gmail.com>
2019-10-13 11:13:19 +0900

    Add ABC094A.cpp

---
+++ solutions/ABC094A.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+using namespace std;
+
+int main() {
+  int a, b, x;
+  cin >> a >> b >> x;
+
+  string ans = "NO";
+  if(a <= x && x <= a + b) {
+    ans = "YES";
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
