817903f6367b675740df284d5b9a7eb18f20b183
Yoshi <crispy245@gmail.com>
2019-10-16 21:43:49 +0900

    Add ABC007A.cpp

---
+++ solutions/ABC007A.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << n - 1 << '\n';
+  return 0;
+}
