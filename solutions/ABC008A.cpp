36fa153dc2f4603f592c92ab23c82fdb6ff9c06b
Yoshi <crispy245@gmail.com>
2019-10-18 21:17:35 +0900

    Add ABC008A.cpp

---
+++ solutions/ABC008A.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int s, t;
+  cin >> s >> t;
+  cout << t - s + 1 << '\n';
+  return 0;
+}
