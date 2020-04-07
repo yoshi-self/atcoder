dbc1ef7754aec4da1371766543c90ae5427e5aeb
Yoshi <crispy245@gmail.com>
2020-04-07 21:45:18 +0900

    Add ABC007A.go

---
+++ solutions/ABC007A.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var n int
+
+	fmt.Fscan(r, &n)
+
+	fmt.Fprintln(w, n - 1)
+}
