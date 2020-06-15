183c1e20592592f32405e9ee720982eb6c31e6f2
Yoshi <crispy245@gmail.com>
2020-06-15 20:11:10 +0900

    Add ABC168A.cpp

---
+++ solutions/ABC168A.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+//const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  int n;
+  cin >> n;
+  string ans;
+  switch(n % 10) {
+    case 3:
+      ans = "bon";
+      break;
+    case 0:
+    case 1:
+    case 6:
+    case 8:
+      ans = "pon";
+      break;
+    default:
+      ans = "hon";
+  }
+  cout << ans << endl;
+  return 0;
+}
