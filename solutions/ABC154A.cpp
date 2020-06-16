437cefc463b8ad835e1b3b6a803496e5b6a40862
Yoshi <crispy245@gmail.com>
2020-06-16 20:19:54 +0900

    Add ABC154A.cpp

---
+++ solutions/ABC154A.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+//const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  map<string, int> mp;
+  string s, t, u;
+  int a, b;
+  cin >> s >> t >> a >> b >> u;
+  mp[s] = a;
+  mp[t] = b;
+  mp[u] -= 1;
+  cout << mp[s] << ' ' << mp[t] << endl;
+
+  return 0;
+}
