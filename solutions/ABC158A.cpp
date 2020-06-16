9039cef36fb663b2d5eeae42cef8caffc4e0c49b
Yoshi <crispy245@gmail.com>
2020-06-16 22:41:56 +0900

    Add ABC158A.cpp

---
+++ solutions/ABC158A.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+//const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  char c, prev;
+  cin >> c;
+  prev = c;
+  REP(i, 3) {
+    cin >> c;
+    if(prev != c) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
