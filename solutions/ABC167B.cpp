e45daadbc7a987fa175636c2c814f8ad41dc9a97
Yoshi <crispy245@gmail.com>
2020-06-17 23:06:42 +0900

    Add ABC167B.cpp

---
+++ solutions/ABC167B.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+//const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  LL a, b, c, k;
+  cin >> a >> b >> c >> k;
+  LL ans = 0;
+
+  if(a >= k) {
+    ans = k;
+  }
+  else if(a + b >= k) {
+    ans = a;
+  }
+  else {
+    ans = a - (k - a - b);
+  }
+  cout << ans << endl;
+  return 0;
+}
+
